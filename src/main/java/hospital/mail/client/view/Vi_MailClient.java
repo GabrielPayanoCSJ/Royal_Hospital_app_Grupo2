c7ff556061295e0a3d7168310d3e7430a8d850d0
JorgeFernandez0901 <jorgefernandezruiz.sanjose@alumnado.fundacionloyola.net>
2021-12-14 21:17:17 +0100

    Panels

--- src/main/java/hospital/mail/client/view/Vi_MailClient.java
+++ src/main/java/hospital/mail/client/view/Vi_MailClient.java
@@ -4,24 +4,56 @@ import java.util.*;
 import javax.swing.*;
 
 public class Vi_MailClient extends JFrame {
-	
-	private ArrayList<JTextField> jtexts;
-	private ArrayList<JButton> jbuttons;
-	
-	
-	
-	public Vi_MailClient() {
-		super("Ventana");
-		this.setSize(800,800);
+	// create Model
+	private ArrayList<JTextField> texts;
+	private ArrayList<JButton> buttons;
+	private JPanel generalPanel;
+	private JPanel emailPanel;
+	private JPanel sidePanel;
+	private JTable inbox;
+
+	public Vi_MailClient(String title) {
+		super(title);
+
+		generalPanel = new JPanel();
+		emailPanel = new JPanel();
+		sidePanel = new JPanel();
+
+		texts = new ArrayList<>();
+		buttons = new ArrayList<>();
+
+		fillButtons();
+
+		createInboxTable();
+		
+//		createPanelLayout();
+		
+		defaultOperations();
+
+	}
+
+	private void defaultOperations() {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.pack();
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+
+	private void createInboxTable() {
+		// change for model's strings
+		Object[] columnName = { "Sender", "Subject" };
+		Object[][] rows = { { "", ""} }; 
+		
 		
 	}
 
-	public static void main(String[] args) {
-		new Vi_MailClient().setVisible(true);
-	
+	private void fillButtons() {
+		// change for model's strings
+		buttons.add(new JButton("Write"));
+		buttons.add(new JButton("Read"));
+		buttons.add(new JButton("Go to FTP"));
+		buttons.add(new JButton("Exit"));
+
 	}
 
 }
-
-
