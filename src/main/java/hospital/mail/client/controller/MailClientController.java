429e63816b2fa181c41b28c28f19311d21962288
Guillermo González <guillermogonzalezdemiguel.sanjose@alumnado.fundacionloyola.net>
2022-01-11 13:46:18 +0100

    update mail

--- src/main/java/hospital/mail/client/controller/MailClientController.java
+++ src/main/java/hospital/mail/client/controller/MailClientController.java
@@ -8,7 +8,6 @@ import hospital.languages.Language;
 import hospital.mail.client.view.JF_MailClient;
 import hospital.mail.client.view.JF_MailLogIn;
 import hospital.mail.server.controller.Utils_Methods;
-import hospital.tools.Tool;
 
 /**
  * Mail client controller, add the events to all buttons through Ev_MailClient
@@ -31,47 +30,24 @@ public class MailClientController {
 		this.loginView = loginView;
 		Language.selectLanguage(language);
 
-		String pass = "";
-		String user = loginView.getTxtFmail().getText();
-		for (int i = 0; i < this.loginView.getPassPassword().getPassword().length; i++) {
-			pass += this.loginView.getPassPassword().getPassword()[i];
-		}
 		try {
-			if (Utils_Methods.userauth(user, pass)) {
-				Utils_Methods.connect(this.loginView.getTxtFmail().getText(), pass);
-				// change to language array
-				clientView = new JF_MailClient("title", "write", "read", "exit", "test", "test", "test");
-				addEvents();
-				fillMails();
-				changeCounts();
-				this.clientView.setVisible(true);
-			}else {
-				Tool.showGUIerror("Contraseña o usuario no válidos", "Fallo de autentificación");
-				loginView.getTxtFmail().setText("");
-				loginView.getPassPassword().setText("");
+			String pass = "";
+			for (int i = 0; i < this.loginView.getPassPassword().getPassword().length; i++) {
+				pass += this.loginView.getPassPassword().getPassword()[i];
 			}
-			
-			
-			
+			System.out.println(pass);
 
-<<<<<<< Updated upstream
-			
-			
-			
-=======
-<<<<<<< HEAD
 			Utils_Methods.connect();
-=======
-			
-			
-			
->>>>>>> 8293f833f22c7ccbd24b835a79ff7edecd5a837a
->>>>>>> Stashed changes
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		
+		// change to language array
+		clientView = new JF_MailClient("title", "write", "read", "exit", "test", "test", "test");
+		addEvents();
+		fillMails();
+		changeCounts();
+		this.clientView.setVisible(true);
 	}
 
 	/**
