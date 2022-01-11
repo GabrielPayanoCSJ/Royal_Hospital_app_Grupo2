aa60d2f20af202ea431c5502017bf73b45dc34a3
JGV97 <javiergomezvillasclaras.sanjose@alumnado.fundacionloyola.net>
2022-01-11 08:35:45 +0100

    Update on Tool and Ev_WriteMail
    Updated both javas.

--- src/main/java/hospital/mail/client/controller/Ev_WriteMail.java
+++ src/main/java/hospital/mail/client/controller/Ev_WriteMail.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import hospital.mail.client.view.JF_MailLogIn;
 import hospital.mail.client.view.JF_MailWrite;
 import hospital.mail.server.controller.Utils_Methods;
 import hospital.tools.Tool;
@@ -21,14 +22,14 @@ import hospital.tools.Tool;
 public class Ev_WriteMail implements ActionListener {
 
 	JF_MailWrite write;
+	JF_MailLogIn log;
 
 	JTextField subject;
-	JTextField from;
+	String from;
 	JTextField to;
 	JTextArea body;
 	String host = "smtp.gmail.com";
-	 String regexPattern ="^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@\" \r\n" +
-	"+ \"[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
+
 	
 	/**
 	 * @param write
@@ -37,15 +38,18 @@ public class Ev_WriteMail implements ActionListener {
 	public Ev_WriteMail(JF_MailWrite write) {
 		this.write = write;
 		subject = write.getBody().getTxtIssue();
-		from = write.getDesc().getTxFDesc().get(0);
+		from = log.getTxtFmail().getText();
 		to = write.getDesc().getTxFDesc().get(1);
 		body = write.getBody().getTxA_body();
 	}
 
+	/**Event handler for the button send E-mail
+	 * 
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if (Pattern.matches(regexPattern,(CharSequence) to)) {
-			Utils_Methods.enviaremail(from.getText(), to.getText(), host, body.getText(), subject.getText());
+		if (Tool.checkEmail(to.getText())) {
+			Utils_Methods.enviaremail(from, to.getText(), host, body.getText(), subject.getText());
 			write.dispose();
 		}else {
 			Tool.showGUIerror("Error destinatario", "Fallo en el formato del email destinatario(Asegurate que es formato *@*.*) "); 
