d8f60fc1542e8a1642dee79c85cf0a853037f76a
JGV97 <javiergomezvillasclaras.sanjose@alumnado.fundacionloyola.net>
2022-01-11 13:06:19 +0100

    Auntetificación Log In
    He añadido la autentificación y el mensaje de error por si fallara.

--- src/main/java/hospital/mail/client/controller/WriteMailController.java
+++ src/main/java/hospital/mail/client/controller/WriteMailController.java
@@ -7,10 +7,12 @@ public class WriteMailController {
 
 	public WriteMailController(String mail) {
 		write = new JF_MailWrite("Send E-mail", "", "test", "Issue", "Message", "From", "To", "Cancel", "Send");
+		 
 		for (int i = 0; i < write.getButtons().getButtons().size(); i++) {
 			write.getButtons().getButtons().get(i).addActionListener(new Ev_WriteMail(write,mail));
 		}
 		
+		write.getDesc().getTxFDesc().get(0).setText(mail);
 		write.setVisible(true);
 	}
 
