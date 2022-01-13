8fbcd63e2613cc55aacf55b9d4c2c284bd30d104
Guillermo González <guillermogonzalezdemiguel.sanjose@alumnado.fundacionloyola.net>
2022-01-13 12:21:22 +0100

    Squashed commit of the following:
    commit 9ea5bd630f2f5e2883f64086b499c8b9e6e2125a
    Author: Guillermo González <guillermogonzalezdemiguel.sanjose@alumnado.fundacionloyola.net>
    Date:   Thu Jan 13 12:18:08 2022 +0100
    
        Update Main View

--- src/main/java/main/controller/EV_Principal.java
+++ src/main/java/main/controller/EV_Principal.java
@@ -7,32 +7,50 @@ import hospital.ftp.client.controller.ClientFTP;
 import hospital.ftp.server.controller.FTPServer;
 import hospital.languages.Language;
 import hospital.mail.client.controller.LoginControllerMail;
-import hospital.mail.client.controller.Pipe_MailUpdater;
-import main.view.JF_Principal;
+import main.view.Jf_Main;
 
 public class EV_Principal implements ActionListener {
-	private JF_Principal view;
+//	private JF_Principal view;
+	private Jf_Main mainView;
 
-	public EV_Principal(JF_Principal view ) {
-		this.view = view;
+//	public EV_Principal(JF_Principal view) {
+//		this.view = view;
+//	}
+
+	public EV_Principal(Jf_Main mainView) {
+		this.mainView = mainView;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+//		// comboBox
+//		if (e.getSource() == view.getJcLanguages()) {
+//			Language.selectLanguage(view.getJcLanguages().getSelectedIndex());
+//		}
+//		
+//		// buttons
+//		if (view.getButtons().get(0) == e.getSource()) {
+//			new FTPServer();
+//		} else if (view.getButtons().get(1) == e.getSource()) {
+//			new ClientFTP();
+//		} else if (view.getButtons().get(2) == e.getSource()) {
+//			new LoginControllerMail();
+//		}
+
 		// comboBox
-		if (e.getSource() == view.getJcLanguages()) {
-			Language.selectLanguage(view.getJcLanguages().getSelectedIndex());
+		if (e.getSource() == this.mainView.getPaMain().getJcLanguages()) {
+			Language.selectLanguage(this.mainView.getPaMain().getJcLanguages().getSelectedIndex());
 		}
-		
+
 		// buttons
-		if (view.getButtons().get(0) == e.getSource()) {
+		if (e.getSource().equals(this.mainView.getPaMain().getButtons().get(0))) {
 			new FTPServer();
-		} else if (view.getButtons().get(1) == e.getSource()) {
+		} else if (e.getSource().equals(this.mainView.getPaMain().getButtons().get(1))) {
 			new ClientFTP();
-		} else if (view.getButtons().get(2) == e.getSource()) {
+		} else if (e.getSource().equals(this.mainView.getPaMain().getButtons().get(2))) {
 			new LoginControllerMail();
 		}
-		
+
 	}
 
 }
